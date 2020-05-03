82c7febf3047f2e243e817d81ef2d34144fac9ff
h0lyduck <pyfsghhz@sina.com>
2020-05-03 17:32:54 +0800

    getUserById

--- Webchat/src/main/java/webchat/dao/UserDao.java
+++ Webchat/src/main/java/webchat/dao/UserDao.java
@@ -8,5 +8,7 @@ public interface UserDao
 {
 	public User login(@Param("User")String User,@Param("UserPass")String Password);
 	
+	public User getUserbyId(@Param("UserId")String UserID);
+	
 	public int registe(@Param("Password")String pass,@Param("UserName")String name,@Param("UserEmail")String email);
 }
